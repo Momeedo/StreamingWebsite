3910259ae24908642a512fa68c1f015aec6f142a
Turki Mohamed <Rishya@Rishyas-MacBook-Pro.local>
2020-07-04 10:39:56 +0100

    Minor changes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,6 +8,7 @@
 		<meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 		<title>Dashboard</title>
+        <link rel="icon" href="{{ asset('/dashboard/img/favicon.ico') }}">
 		<link href="{{ asset('/dashboard/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 		<link href="{{ asset('/dashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
