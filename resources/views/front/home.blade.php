3ce2288cc92146e760016ab26ee9ad5c43da5fbe
Turki Mohamed <Rishya@Rishyas-MacBook-Pro.local>
2020-07-04 11:34:23 +0100

    Minor changes

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -20,7 +20,7 @@
 <section class="vds-main">
 	<div class="container">
 		<div class="vidz_sec">
-			<h3>Highlighted Live Games<a class="h3-seeall" href="{{ url('games') }}">(See All Highlighted Games)</a></h3>
+			<h3>Highlighted Live Games <a class="h3-seeall" href="{{ url('games') }}">(See All Highlighted Games)</a></h3>
 			<div class="vidz_list">
 				<div class="row">
 					
